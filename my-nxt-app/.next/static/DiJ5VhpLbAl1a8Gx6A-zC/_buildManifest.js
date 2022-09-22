683722e91941f734d6c696cdc1b61198367f0973
bcflv-mario <103131627+bcflv-mario@users.noreply.github.com>
2022-09-22 08:34:55 +0200

    fixed link in Home

---
+++ my-nxt-app/.next/static/DiJ5VhpLbAl1a8Gx6A-zC/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-e10102b384efdc34.js"],"/_error":["static/chunks/pages/_error-fb68742d3cf986b6.js"],"/blog":["static/chunks/pages/blog-4ebae8d282208bab.js"],sortedPages:["/","/_app","/_error","/blog"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
